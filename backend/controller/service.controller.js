8bba7b313c7858252b49a764bb8ba8be54ba5fd8
impossible-ad <shirish20602003@gmail.com>
2025-12-09 13:33:03 +0545

    mangsir 23

--- backend/controller/service.controller.js
+++ backend/controller/service.controller.js
@@ -1,48 +1,88 @@
 import db from "../config/dbconnect.js";
 
 export const addService = async (req, res) => {
-    try {
-        const { name, description, address } = req.body;
-        console.log(req.body)
+  try {
+    const { name, description, address } = req.body;
+    console.log(req.body);
 
-        if (!name || !description || !address) {
-            return res.status(400).json({
-                message: "please provide all required data"
-            })
-        };
-
-        await db.execute("INSERT INTO services(name,description,address) VALUES(?,?,?)", [name, description, address]);
-        return res.status(200).json({
-            message: "New Service Added"
-        });
-
-
-
-    } catch (error) {
-        console.log(" Service Failed to Add", error)
+    if (!name || !description || !address) {
+      return res.status(400).json({
+        message: "please provide all required data",
+      });
     }
-}
+
+    await db.execute(
+      "INSERT INTO services(name,description,address) VALUES(?,?,?)",
+      [name, description, address]
+    );
+    return res.status(200).json({
+      message: "New Service Added",
+    });
+  } catch (error) {
+    console.log(" Service Failed to Add", error);
+  }
+};
 
 export const deleteService = async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const [inputedId] = await db.execute("SELECT id FROM services where id=?", [id]);
+    const [inputedId] = await db.execute("SELECT id FROM services where id=?", [
+      id,
+    ]);
 
-        if (inputedId === 0) {
-            return res.status(400).json({
-                message: "no such service found"
-            });
-        }
-
-        await db.execute("DELETE FROM services where id=?", [id]);
-        return res.status(200).json({
-            message: "Service succesfully Deleted"
-        })
-
-
-
-    } catch (error) {
-        console.log(" Service Failed to delete")
+    if (inputedId.length == 0) {
+      return res.status(400).json({
+        message: "no such service found",
+      });
     }
-} 
+
+    await db.execute("DELETE FROM services where id=?", [id]);
+    return res.status(200).json({
+      message: "Service succesfully Deleted",
+    });
+  } catch (error) {
+    console.log(" Service Failed to delete");
+  }
+};
+
+export const editService = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description, address } = req.body;
+
+    const [existing] = await db.execute("SELECT * FROM services WHERE id = ?", [
+      id,
+    ]);
+
+    if (existing.length === 0) {
+      return res.status(400).json({
+        message: "no such service found",
+      });
+    }
+
+    await db.execute(
+      "INSERT INTO services(name,description,address) VALUES(?,?,?)",
+      [name, description, address]
+    );
+
+    return res.status(200).json({
+      message: "New Service Added",
+    });
+  } catch (error) {
+    console.log(" Service Failed to Add", error);
+  }
+};
+
+export const getAllService = async (req, res) => {
+  try {
+    const [result] = await db.execute("SELECT * FROM services ");
+
+    return res.status(200).json({
+      message: "all services are retrieved",
+      data: result,
+    });
+  } catch (error) {
+    console.log("failed to get all services listed");
+  }
+};
--- backend/routes/service.route.js
+++ backend/routes/service.route.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { addService, deleteService } from "../controller/service.controller.js";
+import {
+  addService,
+  deleteService,
+  editService,
+  getAllService,
+} from "../controller/service.controller.js";
 
 const serviceRouter = express.Router();
 
 serviceRouter.post("/addservice", addService);
 serviceRouter.delete("/deleteservice/:id", deleteService);
-
+serviceRouter.patch("/editservice/:id", editService);
+serviceRouter.get("/getallservice", getAllService);
 export default serviceRouter;
